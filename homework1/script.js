d239b2f4d353cc8a0db8b1b8971b750f523396db
kurtskhala <92853046+kurtskhala@users.noreply.github.com>
2024-01-21 13:46:21 +0400

    small changes

--- homework1/script.js
+++
@@ -1,8 +0,0 @@
-let firstname = prompt("შეიყვანე შენი სახელი: ");
-let lastname = prompt("შეიყვენე შენი გვარი: ");
-let birthYear = prompt("შეიყვანე შენი დაბადების წელი: ");
-let address = prompt("შეიყვანე შენი საცხოვრებელი ადგილი: ");
-
-let age = 2024-birthYear;
-
-alert("გამარჯობა "+ firstname + " " + lastname + ", მიხარია რომ უკვე " + age + " წლის ხარ და " + address + "-ში ცხოვრობ");
